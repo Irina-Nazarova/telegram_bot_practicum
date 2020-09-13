c63ee41b3655d6eb7bfd50ddd4d7fc93f9fd4881
Irina <irina.g.nazarova@mail.ru>
2020-09-13 18:57:46 +0300

    done

--- homework.py
+++ homework.py
@@ -11,10 +11,20 @@ PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+URL = "https://praktikum.yandex.ru/api/user_api/homework_statuses/"
+
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    '''
+    {'id': 43314,
+    'status': 'approved',
+    'homework_name': 'Irina-Nazarova__api_01_sms.zip',
+    'reviewer_comment': '',
+    'date_updated': '2020-09-09T18:35:16Z',
+    'lesson_name': 'Отправка SMS-уведомлений'}
+    '''
+    homework_name = homework["homework_name"]
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,26 +32,27 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    params = {'from_date': current_timestamp}
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    homework_statuses = requests.get(URL, headers=headers, params=params)
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
     current_timestamp = int(time.time())  # начальное значение timestamp
-
+    print(time.asctime(time.localtime(time.time())))
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
             current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+            time.sleep(12000)  # опрашивать раз в 20 минут
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
@@ -51,3 +62,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
